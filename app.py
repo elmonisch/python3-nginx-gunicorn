5790e4ea65cc92ec5f25c441665fb25c845fd04d
elmonisch <hasif@twistcode.com>
2019-08-14 13:07:27 +0800

    init

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, jsonify, request
 import torch
 import json
 from torchvision import transforms
-from torchvision.models import resnet34, resnet152
+from torchvision.models import resnet18
 from PIL import Image
 
-model = resnet34(pretrained=True)
+model = resnet18(pretrained=True)
 model.eval()
 
 app = Flask(__name__)
@@ -26,28 +26,32 @@ transform = transforms.Compose([            #[1]
     std=[0.229, 0.224, 0.225]                  #[7]
     )])
 
+@app.route("/hello", methods=['POST'])
+    def hello():
+        return "Hello World!"
+
 @app.route('/classify', methods=['POST'])
 def predict():
-  postedData = {"path": "/home/hasif/personal/python3-nginx-gunicorn/zeb.jpg"}
-  path = postedData["path"]
-  # print(postedData)
-  img = Image.open(path)
+    postedData = {"path": "/home/hasif/personal/python3-nginx-gunicorn/zeb.jpg"}
+    path = postedData["path"]
+    # print(postedData)
+    img = Image.open(path)
 
-  img_t = transform(img)
-  batch_t = torch.unsqueeze(img_t, 0)
+    img_t = transform(img)
+    batch_t = torch.unsqueeze(img_t, 0)
 
-  out = model(batch_t)
+    out = model(batch_t)
 
-  retJson = {}
-  _, indices = torch.sort(out, descending=True)
-  percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
-  # print([(labels[idx], percentage[idx].item()) for idx in indices[0][:5]])
-  # out = [(labels[idx], percentage[idx].item()) for idx in indices[0][:5]]
-  for idx in indices[0][:1]:
-      retJson[f'pred_{i}'] = str("label : {} and confidence : {}".format(labels[idx],percentage[idx].item()))
+    retJson = {}
+    _, indices = torch.sort(out, descending=True)
+    percentage = torch.nn.functional.softmax(out, dim=1)[0] * 100
+    # print([(labels[idx], percentage[idx].item()) for idx in indices[0][:5]])
+    # out = [(labels[idx], percentage[idx].item()) for idx in indices[0][:5]]
+    for idx in indices[0][:1]:
+        retJson[f'pred_{i}'] = str("label : {} and confidence : {}".format(labels[idx],percentage[idx].item()))
 
-  return jsonify(retJson)
+    return jsonify(retJson)
   
   
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True)  
+    app.run(debug=True)  
